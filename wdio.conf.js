93e28dfce614f9e629e12cea0080522490ca1ebf
Ana Maria Cabrera Agudelo <ana.cabrera@veevart.com>
2024-01-18 12:38:56 -0500

    Update and setup

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,53 +1,49 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
+require('dotenv').config();
+
 const { UtamWdioService } = require('wdio-utam-service');
-const PORT = process.env.PORT || 8080;
+// use prefix 'DEBUG=true' to run test in debug mode
+const { DEBUG } = process.env;
+const EXPLICIT_TIMEOUT = 60 * 1000;
+const DEBUG_TIMEOUT = EXPLICIT_TIMEOUT * 30;
 
 exports.config = {
     runner: 'local',
-    specs: ['src/test/**/*.spec.ts'],
+    specs: ['src/test/**/*.spec.js'],
     maxInstances: 1,
-    capabilities: [{
-        maxInstances: 1,
-        browserName: 'chrome'
-    }],
-    logLevel: 'info',
-    //
-    // If you only want to run your tests until a specific amount of tests have failed use
-    // bail (default is 0 - don't bail, run all tests).
+    capabilities: [
+        {
+            maxInstances: 1,
+            browserName: 'chrome',
+            browserVersion: '120.0.6099.62',
+        },
+    ],
+    logLevel: 'debug',
     bail: 0,
-    baseUrl: `http://localhost:${PORT}`,
-    
-    // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
-    
-    // Default timeout in milliseconds for request
-    // if browser driver or grid doesn't send response
+    // timeout for all waitFor commands
+    waitforTimeout: DEBUG ? DEBUG_TIMEOUT : EXPLICIT_TIMEOUT,
     connectionRetryTimeout: 120000,
-    // Default request retries count
     connectionRetryCount: 3,
-
     automationProtocol: 'webdriver',
-    
-    // Test runner services
-    services: ['chromedriver', [UtamWdioService, {}]],
+    services: [
+        [
+            UtamWdioService,
+            {
+                implicitTimeout: 0,
+                injectionConfigs: ['salesforce-pageobjects/ui-utam-pageobjects.config.json'],
+            },
+        ],
+    ],
     framework: 'jasmine',
-    // Test reporter for stdout.
-    // The only one supported by default is 'dot'
-    // see also: https://webdriver.io/docs/dot-reporter.html
     reporters: ['spec'],
-    //
-    // Options to be passed to Jasmine.
-    jasmineNodeOpts: {
-        // Jasmine default timeout
-        defaultTimeoutInterval: 60000,
-        //
-        // The Jasmine framework allows interception of each assertion in order to log the state of the application
-        // or website depending on the result. For example, it is pretty handy to take a screenshot every time
-        // an assertion fails.
-        expectationResultHandler: function(passed, assertion) {
-            // do something
-        }
+    jasmineOpts: {
+        // max execution time for a script, set to 5 min
+        defaultTimeoutInterval: 1000 * 60 * 5,
     },
-    onPrepare() {
-        // Hook before wdio is ready
-    },
-}
+};
